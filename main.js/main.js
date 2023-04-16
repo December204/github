656e0baa5b790608b543b7d8914f2ae839393bcf
pvtai12122004@gmail.com <pvtai12122004@gmail.com>
2023-04-16 23:43:35 +0700

    Add main.js file

---
+++ main.js/main.js
@@ -0,0 +1,90 @@
+var number = 100;
+console.log(number);
+var name = 'tai dep trai vai ca l';
+console.log(name);
+var tau1 =['a', 'b', 'c', 'd', 'e', 'f'];
+console.log(tau1[tau1.length-1]);
+var ismale = 'true';
+var isfemale = 'false' ;
+console.log( ismale );
+var print2 = function print2(){
+    console.log(' taideptrailamaea ');
+}
+function print(){ 
+    console.log('taideptrai')
+    console.log('phamminhtai')
+    console.log('taipham')
+}
+print2=print;
+print();
+var students= {
+    name:'taipham',
+    age :18,
+    university:'FPT'
+}
+console.log(students.university);
+var fullname = "pham"+"minh"+"tai";
+console.log(fullname);
+var number = 6*6;
+console.log (number);
+var number = 6/6.5;
+console.log (number);
+var number = 0;
+console.log(number==1);
+var number = 999
+console.log(number=='999');
+var number = 123;
+console.log(number === '123');
+var x = true 
+var y = !x
+console.log(y);
+var a = 1
+var b = 2
+var c = 3
+console.log( a!= c);
+var x = false && true
+console.log(x);
+var x = 9
+x += 5
+console.log(x);
+var x = 9 
+x ++
+console.log(x);
+var x = 9 
+var y = ++x ;
+console.log(y);
+var x = 16%3 
+console.log(x);
+var a = 3
+if (a = 3) {
+    console.log(1);
+    console.log(2);
+
+}else {
+console.log(3);
+console.log(4);
+
+}
+var score = 8.8 
+if (score<4){
+    console.log('Trung Binh');
+}else if (score < 8){
+    console.log ('kha');
+}else if (score <9){
+    console.log('gioi');
+}else {
+    console.log('excellent')
+
+}
+var score = 9
+if (score <4){
+    console.log('Tb');
+}else if (score>=4 &&  score <8 ){
+    console.log('kha');
+}else if (score>=8 && score <9 ){
+    console.log('gioi')
+}else {
+    console.log ('Excellent');  
+}
+
+
